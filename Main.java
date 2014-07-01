47b7b95cbcbda954c45737fab05b995f93d8607f
Lior <liorshahverdi@gmail.com>
2014-07-01 14:44:38 -0400

    prints matrix

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Main {
+
+	
+	public static int randInt(int min, int max) {
+
+	    // Usually this should be a field rather than a method variable so
+	    // that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+	
+	public static void printMat(int[][] x)
+	{
+		for (int[] row : x)
+		{
+		    for (int value : row)
+		    {
+		    	if (value==0) System.out.print("- ");
+		    	else System.out.print(value+" ");
+		    }
+		    System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] mat = { {0,0,0,0} ,
+						{0,0,0,0} ,
+						{0,0,0,0} ,
+						{0,0,0,0} };
+		
+		
+		printMat(mat);	
+	}
+
+}
