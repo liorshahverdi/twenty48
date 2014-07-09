262aaf5b6dee5624ab441947cc8633947841be07
unknown <liorshahverdi@gmail.com>
2014-07-09 04:03:35 -0400

    could it be

--- Main.java
+++ Main.java
@@ -125,10 +125,10 @@ public class Main {
 		mainGrid.setTile(3,0,8);
 		*/
 
-		mainGrid.setTile(3,2,2);
-		mainGrid.setTile(2,2,4);
-		mainGrid.setTile(1,2,4);
 		mainGrid.setTile(0,2,8);
+		mainGrid.setTile(1,2,8);
+		mainGrid.setTile(2,2,4);
+		mainGrid.setTile(3,2,2);
 
 		printMat(mat);
 		System.out.println("\n---------------------\n");
