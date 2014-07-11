059f72716e13b2ca3904a36253d05e33e6d476db
Lior <liorshahverdi@gmail.com>
2014-07-11 17:10:52 -0400

    copied code toRight, right needs to be transformed

--- Grid.java
+++ Grid.java
@@ -310,14 +310,154 @@ public class Grid {
 	}
 
 	public void shiftRight()
-	{
-	   /*
-	    * 
-		*
-		*
-		*
-		*/
-		
+	{// [row3][colk] -> [rowk][col3]
+	 // must transform
+		for (int k=0; k<4; k++) {
+			if (mat[3][k] != 0) {
+				if (mat[2][k] != 0) {
+					if (mat[3][k] == mat[2][k]) {
+						mat[3][k] += mat[2][k];
+						mat[2][k] = 0;
+						if ( (mat[1][k] != 0) && (mat[0][k] != 0) && (mat[1][k] == mat[0][k]) ) {
+							mat[2][k] = mat[1][k] + mat[0][k];
+							mat[1][k] = 0;
+							mat[0][k] = 0;
+						}
+						else {//row1 != row0
+							if (mat[1][k] == 0) {
+								mat[2][k] = mat[0][k];
+								mat[0][k] = 0;
+							}
+							else {
+								mat[2][k] = mat[1][k];
+								mat[1][k] = mat[0][k]; 
+								mat[0][k] = 0;
+							}
+							
+						}
+					}
+					else if (mat[2][k] == mat[1][k]) {
+						mat[2][k] += mat[1][k];
+						if (mat[0][k] != 0) {
+							mat[1][k] = mat[0][k];
+							mat[0][k] = 0;
+						}
+						else {
+							mat[1][k] = 0;
+						} 
+					}
+					else if ((mat[2][k] == mat[0][k]) && (mat[1][k]==0)) {
+						mat[2][k] += mat[0][k];
+						mat[0][k] = 0;
+					}
+					else if (mat[1][k] == mat[0][k]) {
+						mat[1][k] += mat[0][k];
+						mat[0][k] = 0;
+					}
+				}	
+				else if (mat[1][k] != 0) {
+					if (mat[3][k] == mat[1][k]) {
+						mat[3][k] += mat[1][k];
+						mat[1][k] = 0;
+						if (mat[0][k] != 0) {
+							mat[2][k] = mat[0][k];
+							mat[0][k] = 0;
+						}
+					} 
+					else {//[3][k]taken [2][k]empty [1][k]taken
+						if (mat[1][k] == mat[0][k]) {
+							mat[2][k] = mat[1][k] + mat[0][k];
+							mat[1][k] = 0;
+							mat[0][k] = 0;
+						}
+						else {
+							mat[2][k] = mat[1][k];
+							mat[1][k] = 0;
+						}
+					}
+				}
+				else if (mat[0][k] != 0) {
+					if (mat[3][k] == mat[0][k]) {
+						mat[3][k] += mat[0][k];
+						mat[0][k] = 0;
+					}
+					else {
+						mat[2][k] = mat[0][k];
+						mat[0][k] = 0;
+					} 
+				}
+			}
+			else {//mat[3][k] empty
+				if (mat[2][k] != 0) {
+					if (mat[1][k] != 0) {
+						if (mat[2][k] == mat[1][k]) {
+							mat[3][k] = mat[2][k] + mat[1][k];
+							mat[2][k] = mat[0][k];
+							mat[1][k] = 0;
+							mat[0][k] = 0;
+						}
+						else {//[2][k] != [1][k] AND [3][k] is empty
+							if (mat[0][k] != 0) {
+								if (mat[1][k] == mat[0][k]) {
+									mat[3][k] = mat[2][k];
+									mat[2][k] = mat[1][k] + mat[0][k];
+									mat[1][k] = 0;
+									mat[0][k] = 0;
+								}
+								else {
+									mat[3][k] = mat[2][k];
+									mat[2][k] = mat[1][k];
+									mat[1][k] = mat[0][k];
+									mat[0][k] = 0;
+								}
+							}
+							else {
+								mat[3][k] = mat[2][k];
+								mat[2][k] = mat[1][k];
+								mat[1][k] = 0;
+							}			
+						}
+					}
+					else {//[3][k] empty, [2][k] occupied, [1][k] empty
+						if (mat[2][k] == mat[0][k]) {
+							mat[3][k] = mat[2][k] + mat[0][k];
+							mat[2][k] = 0;
+							mat[0][k] = 0;
+						}
+						else {
+							mat[3][k] = mat[2][k];
+							mat[2][k] = mat[0][k];
+							mat[0][k] = 0;
+						}
+					}
+				}
+				else {//mat[2][k] AND mat[3][k] empty
+					if (mat[1][k] != 0) {
+						if (mat[0][k] != 0) {
+							if (mat[1][k] == mat[0][k]) {
+								mat[3][k] = mat[1][k] + mat[0][k];
+								mat[1][k] = 0;
+								mat[0][k] = 0;
+							}
+							else {
+								mat[3][k] = mat[1][k];
+								mat[2][k] = mat[0][k];
+								mat[1][k] = 0;
+								mat[0][k] = 0;
+							}
+						}
+						else {
+							mat[3][k] = mat[1][k];
+							mat[1][k] = 0;
+						}
+					} 
+					else {
+						mat[3][k] = mat[0][k];
+						mat[0][k] = 0;
+					}
+				}
+			}	
+		}	
 	}
 	
 	public void shiftLeft()
